e27630d2af47cbdd589f87cca90bdb677a368b69
shuaiwang516 <shuaiwang516@gmail.com>
2022-08-28 20:09:52 -0500

    prepare artifacts

--- experiment/run.sh
+++ experiment/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+mode=$1
+project=$2
+
 function usage() {
-    echo 'Usage: ./run.sh [mode] [project]'
-    echo '[mode]: (1) urts (2) retestall'
+    echo 'Usage: ./run_azure.sh [mode] [project] [sha1] [sha2]'
+    echo '[mode]: (1) urts (2) retestall (3) ekstazi-ext (4) ekstazi-unsafe'
     echo '[project]: (1) hcommon (2) hbase (3) hdfs (4) alluxio (5) zookeeper'
     exit 1
 }
@@ -12,24 +15,19 @@ function runExperiment() {
     then
         cd $mode/$project/
         echo '============== Start Running '$mode' '$project' =============='
-        python3 run.py
+        python3 run_azure.py | tee output.out
         cd ../..
         echo '============== Finish Running '$mode' '$project' =============='
-        echo '============== Extracting '$mode' '$project' data =============='
-        python3 parse_result.py $mode $project
         echo 'Done!'
     else
         usage
     fi
 }
 
-mode=$1
-project=$2
-
 function main() {
-    if [ -z $mode ] || [ -z $project ]; then
+    if [ -z $mode ] || [ -z $project ] || [ -z $sha1 ] || [ -z $sha2 ]; then
         usage
-    elif [ $mode = "urts" ] || [ $mode = "retestall" ]; then
+    elif [ $mode = "urts" ] || [ $mode = "retestall" ] || [ $mode = "ekstazi-ext" ] || [ $mode = "ekstazi-unsafe" ]; then
         runExperiment
     else
         usage
